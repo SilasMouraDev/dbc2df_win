8fc8aeb4c6428e135f89ed8f7e462fa3b333de20
SilasMouraDev <silas_moura_5@hotmail.com>
2020-08-12 00:13:14 -0300

    setup update

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
-with open(".\\bdc2df_win\\README.md","r") as fh:
+with open(".\\dbc2df_win\\README.md","r") as fh:
     long_description=fh.read()
 
 setuptools.setup(
-    name='bdc2df_win',  
+    name='dbc2df_win',  
     version='0.1',
     author="SilasMouraDev",
     author_email="silas_moura_5@hotmail.com",
     description="Utility for loading dbc files into Pandas DataFrames",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/SilasMouraDev/bdc2df_win",
+    url="https://github.com/SilasMouraDev/dbc2df_win",
     packages=setuptools.find_packages(),
     install_requires=['dbfread','pandas'],
     include_package_data=True,
