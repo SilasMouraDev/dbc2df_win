f3b296f319c4a8050f0be964e64cf92a557790a8
SilasMouraDev <silas_moura_5@hotmail.com>
2020-08-12 10:16:55 -0300

    setup.py update

--- build/lib/dbc2df_win/__init__.py
+++ build/lib/dbc2df_win/__init__.py
@@ -11,13 +11,13 @@ if platform.machine().endswith('64'):
      blast_dbf = os.path.join(lib_fold,'blast-dbf_64.exe')
 else:blast_dbf = os.path.join(lib_fold,'blast-dbf_32.exe')
 
-def open_dbc(src_path):
+def open_dbc(src_path,encod='latin-1'):
     temp_file = ''.join(random.choice('0123456789ABCDEF') for i in range(16))+'.dbf'
     temp_path = os.path.join(os.environ['TEMP'],temp_file)
  
     sp.run('"'+blast_dbf+'" "'+src_path+'" "'+temp_path+'"')
 
-    dbf = dbfr.DBF(temp_path)
+    dbf = dbfr.DBF(temp_path,encoding=encod)
     df = pd.DataFrame(iter(dbf))
     
     os.remove(temp_path)
